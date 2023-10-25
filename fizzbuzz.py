a97df48f62bf4551dcf0181998b9b4c3e6fbcf33
James Flood Jr <floodadosious111@gmail.com>
2023-10-25 11:25:53 -0400

    added if statemetns

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,11 @@
 # add your code here
 userNumber = int(input("Please enter a number: "))
+
+if userNumber % 3 == 0 and userNumber % 5 ==0:
+    print("FizzBuzz")
+elif userNumber % 3 == 0:
+    print("Fizz")
+elif userNumber % 5 == 0:
+    print("Buzz")
+else:
+    print(userNumber, "is not Fizz or Buzz")
