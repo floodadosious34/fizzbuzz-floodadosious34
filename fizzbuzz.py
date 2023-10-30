aae5793509e7ed381bdc4f2ec6d36a51ba460e6d
James Flood Jr <floodadosious111@gmail.com>
2023-10-29 22:20:39 -0400

    finished fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,10 @@
 # add your code here
-userNumber = int(input("Please enter a number: "))
-
-if userNumber % 3 == 0 and userNumber % 5 ==0:
-    print("FizzBuzz")
-elif userNumber % 3 == 0:
-    print("Fizz")
-elif userNumber % 5 == 0:
-    print("Buzz")
-else:
-    print(userNumber, "is not Fizz or Buzz")
+for i in range(1, 100):
+    if i % 3 == 0 and i % 5 ==0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
